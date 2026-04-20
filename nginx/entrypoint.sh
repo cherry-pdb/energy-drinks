4cad134733b02fb8ca1ef055eac2694e308d6b6d
cherry-pdb <mark.v.nazarov@yandex.ru>
2026-04-21 01:48:52 +0300

    fixed http-redirecting

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -5,16 +5,18 @@ set -eu
 
 HTTP_TEMPLATE="/etc/nginx/templates/http.conf.template"
 HTTPS_TEMPLATE="/etc/nginx/templates/https.conf.template"
+HTTP_TLS_REDIRECT_TEMPLATE="/etc/nginx/templates/http.tls-redirect.conf.template"
 OUTPUT="/etc/nginx/conf.d/default.conf"
-
-envsubst '${DOMAIN}' < "$HTTP_TEMPLATE" > "$OUTPUT"
 
 CERT_PATH="/etc/letsencrypt/live/${DOMAIN}/fullchain.pem"
 KEY_PATH="/etc/letsencrypt/live/${DOMAIN}/privkey.pem"
 
 if [ -f "$CERT_PATH" ] && [ -f "$KEY_PATH" ]; then
+  envsubst '${DOMAIN}' < "$HTTPS_TEMPLATE" > "$OUTPUT"
   echo "" >> "$OUTPUT"
-  envsubst '${DOMAIN}' < "$HTTPS_TEMPLATE" >> "$OUTPUT"
+  envsubst '${DOMAIN}' < "$HTTP_TLS_REDIRECT_TEMPLATE" >> "$OUTPUT"
+else
+  envsubst '${DOMAIN}' < "$HTTP_TEMPLATE" > "$OUTPUT"
 fi
 
 nginx -g 'daemon off;'
